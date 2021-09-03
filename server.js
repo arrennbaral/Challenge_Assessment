e2bb3509fc591e7216532169e741f650f25c291c
Arrenn Baral <54922563+arrennbaral@users.noreply.github.com>
2021-09-03 13:42:01 -0700

    get

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', "*");
+    next();
+  }
+  app.use(allowCrossDomain);
+
 
 
 app.use('/get', require('./routers/routes'))
