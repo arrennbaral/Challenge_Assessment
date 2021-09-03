d5d2ff9f85ad696dc8b1c02293c1961cde894d31
Arrenn Baral <54922563+arrennbaral@users.noreply.github.com>
2021-09-03 13:01:01 -0700

    key updates

--- server.js
+++ server.js
@@ -18,8 +18,9 @@ app.use('/get', require('./routers/routes'))
 
 
 
-app.listen(4000,()=>console.log("Server running on port 4000"))
-
-
+const PORT = process.env.PORT || 4000
+app.listen(PORT, ()=>{
+    console.log('Server is running on port', PORT)
+})
 
 
