96c770d442b445fece095aedf0c6a27e87e43adb
Arrenn Baral <54922563+arrennbaral@users.noreply.github.com>
2021-09-03 13:08:44 -0700

    NODE_ENV update

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ app.use('/get', require('./routers/routes'))
 
 
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 
 
@@ -22,5 +28,7 @@ const PORT = process.env.PORT || 4000
 app.listen(PORT, ()=>{
     console.log('Server is running on port', PORT)
 })
+
+
 
 
